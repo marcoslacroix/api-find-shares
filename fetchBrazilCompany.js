ce0ea507e11614cfa3863191f7bd620d51a7e985
Marcoslacroix <marcos.lacroix@tdsoft.com.br>
2023-06-30 09:21:53 -0300

    removido campos desnecessários do fundo imobiliario e adicionado logica de liquidez diaria > 1M e margembit > 0

--- fetchBrazilCompany.js
+++ fetchBrazilCompany.js
@@ -17,7 +17,7 @@ functionUtils.getStocksInfo(urlGetStocksInfo).then((results) => {
             functionUtils.getRevenue({ ticker: item.ticker, url: urlGetRevenue }).then((result) => {
                 const wasProfitNegative = functionUtils.checkIfItHasNegativeProfitInTheLast10Years(result);
                 functionUtils.getStockPageInfo({ticker: item.ticker, url: urlGetStockPageInfo}).then((stockPageInfo) => {
-                    if ((stockPageInfo.tagAlong === '100 %' || stockPageInfo.tagAlong === '80 %')  && !wasProfitNegative && item.liquidezmediadiaria > 1000000 && item.liquidezmediadiaria != null) {
+                    if ((stockPageInfo.tagAlong === '100 %' || stockPageInfo.tagAlong === '80 %')  && !wasProfitNegative && item.liquidezmediadiaria > 1000000 && item.liquidezmediadiaria != null && item.margemebit > 0) {
                       functionUtils.getEbitValue({ticker: item.ticker, url: urlGetEbitValue}).then((response) => {
                           let ebit = functionUtils.parseEbitValue(response);
                           let vi = Math.sqrt(22.5 * item.lpa * item.vpa);
--- models.js
+++ models.js
@@ -196,119 +196,16 @@ const realEstateFunds = {
     type: Sequelize.FLOAT,
     allowNull: false
   },
-  p_l: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
   dy: {
     type: Sequelize.FLOAT,
     allowNull: false
-  },
-  tagAlong: {
-    type: Sequelize.STRING,
-    allowNull: true
   },
   p_vp: {
     type: Sequelize.FLOAT,
     allowNull: false
   },
-  p_ebit: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  p_ativo: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  ev_ebit: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  margembruta: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  margemebit: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  margemliquida: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  p_sr: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  p_capitalgiro: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  p_ativocirculante: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  giroativos: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  roe: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  roa: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  roic: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  dividaliquidapatrimonioliquido: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  dividaLiquidaebit: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  pl_ativo: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  passivo_ativo: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  liquidezcorrente: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  peg_ratio: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  receitas_cagr5: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  lucros_cagr5: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
+
   liquidezmediadiaria: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  vpa: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  lpa: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  valormercado: {
     type: Sequelize.FLOAT,
     allowNull: false
   },
@@ -334,18 +231,7 @@ const realEstateFunds = {
   },
   sectorname: {    
     type: Sequelize.STRING,
-    allowNull: false},
-  vi: {
-    type: Sequelize.FLOAT,
     allowNull: false
-  },
-  percent_more: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
-  earningYield: {
-    type: Sequelize.FLOAT,
-    allowNull: true
   },
   dyMediana: {
     type: Sequelize.FLOAT,
